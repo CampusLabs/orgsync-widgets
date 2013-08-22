bc5280e5755c23ddbdad35308407684665f6ac2a
Casey Foster <c@sey.me>
2013-08-22 11:41:17 -0500

    Use cogs to concat to one big file

--- orgsync-widgets.js
+++ orgsync-widgets.js
@@ -1,3 +1,14 @@
+//= require jquery/jquery
+//= require underscore/underscore
+//= require underscore.string/lib/underscore.string
+//= require underscore-inherit/underscore-inherit
+//= require backbone/backbone
+//= require backbone-relations/backbone-relations
+//= require select2/select2.js
+//= require olay/olay.js
+//= require async/lib/async
+//= require orgsync-javascript-api/orgsync-javascript-api
+//= require dpr/dpr.js
 //= requireSelf
 //= requireTree models
 //= requireTree jst
@@ -8,6 +19,7 @@
 
   var $ = window.jQuery;
   var _ = window._;
+  var dpr = window.dpr;
 
   // Define our global namespace.
   var app = window.OrgSyncWidgets = {
@@ -20,6 +32,7 @@
     // In the ready function, run through the selectorViewMap and initialize
     // views accordingly.
     ready: function () {
+      $('html').addClass('dpr-' + dpr());
       if (!app.api.key) return app.authorize();
       _.each(app.selectorViewMap, function (view, selector) {
         $(selector).each(function () { new view({el: this}); });

d1735089e6660ed868ef4305b065cc46df83ba2c
Casey Foster <c@sey.me>
2013-08-24 13:04:18 -0500

    Unique by name in umbrella/category selectors

--- views/portals/browse/index.js
+++ views/portals/browse/index.js
@@ -98,9 +98,11 @@
       var $el = this.$('.js-' + singular + '-selector');
       if (models.length <= 1) return $el.hide();
       $el.select2({
-        data: models.map(function (model) {
-          return {id: model.id, text: model.get('name')};
-        }),
+        data: _.map(models.reduce(function (data, model) {
+          var name = model.get('name');
+          if (!data[name]) data[name] = {id: name, text: name};
+          return data;
+        }, {}), _.identity),
         placeholder: 'Filter by ' + _.str.capitalize(singular),
         minimumResultsForSearch: 5,
         allowClear: true
@@ -176,8 +178,8 @@
       this.filtered.set(
         this.portals.filter(function (portal) {
           return portal.matchesQuery(query) &&
-            (!umbrellaId || portal.get('umbrella_id') == umbrellaId) &&
-            (!categoryId || portal.get('category_id') == categoryId) &&
+            (!umbrellaId || portal.get('umbrella').get('name') == umbrellaId) &&
+            (!categoryId || portal.get('category').get('name') == categoryId) &&
             (!matcher || matcher.test(portal.get('name') || ''));
         })
       );

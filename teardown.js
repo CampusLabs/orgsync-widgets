d4821900c817e179ec1eb4084732726a5c7929c0
Casey Foster <c@sey.me>
2013-09-16 19:54:37 -0500

    Store references to dependencies on the OrgSyncWidgets object

--- teardown.js
+++ teardown.js
@@ -1,11 +1,14 @@
 // Close and execute the IIFE that was started in setup.js. Here we return all
 // of the overriden globals to their previous states.
 
+  var app = window.OrgSyncWidgets;
+
   // This local delcaration is important to store for the Underscore templates
   // since they use `_.escape`.
   var _ = window._;
 
   _.each(polutants, function (val, key) {
+    app[key] = window[key];
     if (val) return window[key] = val;
     delete window[key];
   });

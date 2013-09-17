243f9bf5ea1fa719b96b926ec13fbc9092bbe064
Casey Foster <c@sey.me>
2013-09-16 20:20:59 -0500

    Make it prettier

--- themes/orgsync/js/views/albums/browse/list-item.js
+++ themes/orgsync/js/views/albums/browse/list-item.js
@@ -1,16 +1,18 @@
 (function () {
   'use strict';
 
-  var _ = window.OrgSyncWidgets._;
+  var app = window.OrgSyncWidgets;
 
-  var AlbumsBrowseListItemView = window.OrgSyncWidgets.AlbumsBrowseListItemView;
+  var $ = app.jQuery;
+  var _ = app._;
+
+  var AlbumsBrowseListItemView = app.AlbumsBrowseListItemView;
   var render = AlbumsBrowseListItemView.prototype.render;
-
 
   AlbumsBrowseListItemView.prototype.render = function () {
     render.apply(this, arguments);
-    var $img = this.$('img');
-    this.$el.append(_.times(3, _.bind($img.clone, $img)));
+    var $img = this.$('img').wrap($('<div>').addClass('js-stack-container'));
+    $img.before(_.times(3, _.bind($img.clone, $img)));
     return this;
   };
 })();

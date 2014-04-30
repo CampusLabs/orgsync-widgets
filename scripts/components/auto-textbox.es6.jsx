33aaf292cd3b93f170298e93b2817dc7ba8c47cd
Casey Foster <c@sey.me>
2014-04-30 09:52:07 -0500

    AutoGrowInput -> AutoTextbox

---
+++ scripts/components/auto-textbox.es6.jsx
@@ -0,0 +1,33 @@
+/** @jsx React.DOM */
+
+import $ from 'jquery';
+import React from 'react';
+
+export default React.createClass({
+  componentDidMount: function () {
+    $(window).on('resize', this.resize);
+    this.resize();
+  },
+
+  componentWillUnmount: function () {
+    $(window).off('resize', this.resize);
+  },
+
+  componentDidUpdate: function () {
+    this.resize();
+  },
+
+  resize: function () {
+    var $el = $(this.getDOMNode());
+    $el.height(0);
+    var targetHeight = $el.prop('scrollHeight');
+    var clientHeight = $el.prop('clientHeight');
+    var innerHeight = $el.innerHeight();
+    if (clientHeight === innerHeight) targetHeight -= clientHeight;
+    $el.height(targetHeight);
+  },
+
+  render: function () {
+    return this.transferPropsTo(<textarea />);
+  }
+});
